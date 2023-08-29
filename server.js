737c1979622e2ccdaff690b8e9bdbf56ac0bfdb9
Connor <101947931+CARhoden@users.noreply.github.com>
2023-08-29 17:43:43 -0500

    started models and routes

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const userRoutes = require('./routes/userRoutes');
 
 require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(userRoutes);
+
+
+const PORT = process.env.PORT || 3001;
 
 // connect to MONGO_URL from .env
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+    useUnifiedTopology: true
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
 });
